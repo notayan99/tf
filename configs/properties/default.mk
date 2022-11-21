1cea991b29a12020464c52b8cab5cbddd6f7beba
ryyaap. (unlstedd) <rktdnt@proton.me>
2022-11-21 22:26:08 +0700

    chime: Sync SurfaceFlinger prop from cheetah
    Signed-off-by: ryyaap. (unlstedd) <rktdnt@proton.me>

--- configs/properties/default.mk
+++ configs/properties/default.mk
@@ -16,23 +16,39 @@
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.has_wide_color_display=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.set_idle_timer_ms=2000 \
+    ro.surface_flinger.enable_layer_caching=true \
+    ro.surface_flinger.set_idle_timer_ms=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.surface_flinger.wcg_composition_dataspace=143261696 \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.use_color_management=true \
-    ro.surface_flinger.wcg_composition_dataspace=143261696
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.display_update_imminent_timeout_ms=50 \
+    ro.surface_flinger.support_kernel_idle_timer=true \
+    ro.surface_flinger.ignore_hdr_camera_layers=true \
+    vendor.hwc.dpp.downscale=2
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.enable_advanced_sf_phase_offset=1 \
     debug.sf.high_fps_early_gl_phase_offset_ns=-2000000 \
     debug.sf.high_fps_early_phase_offset_ns=-5000000 \
     debug.sf.high_fps_late_app_phase_offset_ns=1000000 \
-    debug.sf.high_fps_late_sf_phase_offset_ns=-2000000
+    debug.sf.high_fps_late_sf_phase_offset_ns=-2000000 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=16600000 \
+    debug.sf.early.sf.duration=16600000 \
+    debug.sf.early.app.duration=16600000 \
+    debug.sf.earlyGl.sf.duration=16600000 \
+    debug.sf.earlyGl.app.duration=16600000 \
+    debug.sf.frame_rate_multiple_threshold=120 \
+    debug.sf.layer_caching_active_layer_timeout_ms=1000 \
+    debug.sf.treat_170m_as_sRGB=1
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
